df98e059140d42d35587b2d76ca7db7765bd8225
bevennyamande <bevennyamande@gmail.com>
2019-04-30 15:03:48 +0200

    commits

--- run.py
+++ run.py
@@ -2,7 +2,19 @@ import tweepy
 import pygal
 from flask import Flask
 
+consumer_key = 'Q47knI9nZpfMzKm0fcDWd'
+consumer_secret = 'S6D5lr3WbrsVB99JQhl4w1Yng4NsgvNlZtbKVSEUHDiEkOjyV0'
+access_token = '63067026-02RUqz1cnkdkoLokoxZGSZUnHMWe0c6r3GmxNwd5o'
+access_token_secret = 'X7KX0NiqAZ9bc3qHXmmGUYOvwxsOpMVOxlmk7N1d6ZGO'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+user = api.get_user('werkzeug')
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -10,4 +22,5 @@ def index():
 
 
 if __name__ == '__main__':
+    print(user)
     app.run(debug=True)
